3dbd8ee54c0108f7965fcb131e6c0bd3e8f2c344
Nawab Moazzam <cnstatus310@gmail.com>
2025-11-07 23:42:13 +0500

    fix: Improve error handling and dynamic fetching for favicon

--- client/app/favicon.ico/route.ts
+++ client/app/favicon.ico/route.ts
@@ -1,26 +1,37 @@
+import { getStrapiMedia } from "@/components/strapi-image";
 import { getGlobalData } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
-  const global = await getGlobalData();
-  const faviconUrl = global.favicon.url;
+  try {
+    const global = await getGlobalData();
+    const faviconUrl = global.favicon.url;
 
-  if (!faviconUrl) {
-    // Fallback to default favicon
-    return new NextResponse(null, { status: 404 });
+    if (!faviconUrl) {
+      // Fallback to default favicon
+      return new NextResponse(null, { status: 404 });
+    }
+
+    const faviconResponse = await fetch(
+      `${getStrapiMedia(faviconUrl)}`
+    );
+
+    const faviconBuffer = await faviconResponse.arrayBuffer();
+
+    return new NextResponse(faviconBuffer, {
+      headers: {
+        "Content-Type":
+          faviconResponse.headers.get("Content-Type") || "image/x-icon",
+        "Cache-Control": "public, max-age=86400, immutable", // Cache for 24 hours
+      },
+    });
+  } catch (error) {
+    console.error(`Error Fetching Global Data: ${error}`);
+    return NextResponse.json(
+      { error: "Failed to fetch global data" },
+      { status: 500 }
+    );
   }
-
-  const faviconResponse = await fetch(
-    `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${faviconUrl}`
-  );
-
-  const faviconBuffer = await faviconResponse.arrayBuffer();
-
-  return new NextResponse(faviconBuffer, {
-    headers: {
-      "Content-Type":
-        faviconResponse.headers.get("Content-Type") || "image/x-icon",
-      "Cache-Control": "public, max-age=86400, immutable", // Cache for 24 hours
-    },
-  });
 }
